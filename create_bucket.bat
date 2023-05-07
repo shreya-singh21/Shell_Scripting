588cbf595011bf0c89003215702c783aea02fa67
shreya-singh21 <shreyasinghf21@gmail.com>
2023-05-07 13:58:26 +0530

    Initial commit

---
+++ create_bucket.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the name of your new S3 bucket
+BUCKET_NAME="my-bucketghj"
+
+# Set the region for your new S3 bucket
+BUCKET_REGION="ap-south-1"
+
+# Create the new S3 bucket
+aws s3api create-bucket \
+--bucket $BUCKET_NAME \
+--region $BUCKET_REGION \
+--create-bucket-configuration LocationConstraint=$BUCKET_REGION
+
+# Confirm the new bucket was created
+echo "S3 bucket $BUCKET_NAME created in region $BUCKET_REGION."
